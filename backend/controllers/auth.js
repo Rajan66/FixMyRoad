7f5cff71e9cd421b57cf85daf15ad03298b0b4b7
Rajan66 <rajanmaharjan042@gmail.com>
2024-12-20 23:55:30 +0545

    feat: add complaint route and controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -97,11 +97,6 @@ const createToken = async (userId, res) => {
 };
 
 exports.isAuthenticated = (req, res, next) => {
-  const token = req.headers["authorization"];
-  console.log(token)
-  console.log(req.profile)
-  console.log(req.auth)
-
   let checker = req.profile && req.auth && req.profile._id == req.auth._id;
   if (!checker) {
     return res.status(403).json({
