71775c0f9d5e42ecca3b870d5f93f28b23082035
Rajan66 <rajanmaharjan042@gmail.com>
2024-12-20 23:03:00 +0545

    feat: implement and test report functionalities

---
+++ backend/routes/report.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const {
+  createReport,
+  getAllReports,
+  getReportById,
+  updateReport,
+  deleteReport,
+  getReportsNearLocation,
+  getReportsByAddress,
+} = require("../controllers/report");
+const { isSignedIn, isAuthenticated } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
+
+const router = express.Router();
+
+// --- params ----
+router.param("userId", getUserById);
+
+// --- http routes ---
+router.post("/reports", isSignedIn, createReport);
+
+router.get("/reports", getAllReports);
+
+router.get("/reports/near", getReportsNearLocation);
+
+router.get("/reports/:reportId", getReportById);
+
+router.get("/reports/address", getReportsByAddress);
+
+router.patch("/reports/:reportId", updateReport);
+
+router.delete("/reports/:reportId", deleteReport);
+
+module.exports = router;
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const authRoute = require("./auth");
+const reportRoute = require("./report");
 
 router.use(authRoute);
+router.use(reportRoute);
 
 module.exports = router;
