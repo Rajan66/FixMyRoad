06a8d51cdd31a8b72b233e65764088e1796df80b
Aayush Prajapati <aayush.prajapati0430@gmail.com>
2024-12-21 17:50:28 +0545

    reportcart

--- frontend/app/(main)/_components/ReportCard.tsx
+++ frontend/app/(main)/_components/ReportCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CircleUserRound } from "lucide-react";
+import Link from "next/link";
 
 const ReportCard = () => {
   return (
@@ -12,9 +13,9 @@ const ReportCard = () => {
         <img
           src="https://images.squarespace-cdn.com/content/v1/573365789f726693272dc91a/1704992146415-CI272VYXPALWT52IGLUB/AdobeStock_201419293.jpeg?format=1500w"
           alt="image"
-          className="h-[90%] w-[70%]"
+          className="h-[100%] w-[100%] rounded-lg"
         />
-        <div className="flex flex-col h-[90%] ml-1">
+        {/* <div className="flex flex-col h-[90%] ml-1">
           <img
             src="https://cdn.shopify.com/s/files/1/0274/7288/7913/files/MicrosoftTeams-image_32.jpg?v=1705315718"
             alt=""
@@ -25,15 +26,15 @@ const ReportCard = () => {
             alt=""
             className="h-[50%] w-max mt-1"
           />
-        </div>
+        </div> */}
       </div>
       <div className="mt-2">Paknajol, Kathmandu</div>
       <div className="mt-2">Votes: 10</div>
-      <a href="ReportPage.tsx">
+      <Link href="/reportPage">
         <div className="absolute  bottom-8 right-8 bg-gray-300 p-2 rounded-sm hover:scale-105">
           View More
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
