079c14d8cc3df7ec6553fe2e6efc7c5ab695f8ed
Rajan66 <rajanmaharjan042@gmail.com>
2024-12-21 07:38:42 +0545

    feat: add and modify model and modify the report controller

---
+++ backend/models/cluster.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const clusterSchema = new mongoose.Schema(
+  {
+    location: {
+      type: String,
+      required: true,
+    },
+    reports: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Report",
+      },
+    ],
+    reportCount: {
+      type: Number,
+      default: 0,
+    },
+    aggregatedSeverity: {
+      type: String,
+      enum: ["Minor", "Moderate", "Severe"],
+      default: "Minor",
+    },
+    isValid: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Middleware to update `updatedAt` field on document modification
+clusterSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+module.exports = mongoose.model("Cluster", clusterSchema);
