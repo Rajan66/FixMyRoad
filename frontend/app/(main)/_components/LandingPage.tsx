5e78926f8f0722d324cbb394dbed5ecda5c10279
Aayush Prajapati <aayush.prajapati0430@gmail.com>
2024-12-21 14:50:45 +0545

    landing

--- frontend/app/(main)/_components/LandingPage.tsx
+++ frontend/app/(main)/_components/LandingPage.tsx
@@ -4,12 +4,17 @@ import React from "react";
 const LandingPage = () => {
   return (
     <div>
-      <div className="bg-gray-100 h-max w-[90vw] mx-10 my-5 rounded-lg poppins italic text-3xl text-gray-500 font-light pl-[30vw] pt-8 pb-8">
+      <div className="bg-gray-100 h-max w-[90vw] mx-[5vw] my-5 rounded-lg italic text-3xl text-gray-500  pl-[30vw] pt-8 pb-8">
         "Your Road, Our Responsibility!"
       </div>
-      <div className="bg-gray-100 h-max w-[90vw] rounded-lg poppins p-8">
-        <p className="text-xl poppins mb-2">Recent Reports</p>
-        <div>
+      <div className="bg-gray-100 h-max w-[90vw] rounded-lg poppins p-8 mx-[5vw] my-5">
+        <p className="text-3xl poppins mb-5 text-center font-light">Reports</p>
+        <div className="grid grid-cols-3 gap-y-5">
+          <ReportCard />
+          <ReportCard />
+          <ReportCard />
+          <ReportCard />
+          <ReportCard />
           <ReportCard />
         </div>
       </div>
